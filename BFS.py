76d8239304c8c59e1597b063fde6021f3e95bb3e
Minh <hbminh97@gmail.com>
2022-04-04 10:08:07 +0700

    Update

--- BFS.py
+++ BFS.py
@@ -6,15 +6,7 @@ class Board:
         self.size=size
         self.Blacks=[]
         self.Whites=[]
-        self.parent=None
-        self.change=[]
-        self.heuristic=[]
-    def setParent(self,parent):
-        self.parent=parent
-    def setChange(self,x,y):
-        self.change=[x,y]
-    def setHeuristic(self,x,y):
-        self.heuristic=[x,y]
+
     def compareToOther(self,otherBoard):
         for white in self.Whites:
             otherWhite=otherBoard.findCell(white.x,white.y)
@@ -49,19 +41,6 @@ class Board:
     def lookAround(self,x,y):
         pass
 
-    def printOut1(self):
-        print("-----------------------------------")
-        for y in range(1,self.size+1):
-            for x in range(1,self.size+1):
-                cell=self.findCell(x, y)
-                if cell is not None and cell.isBlack:
-                    if cell.isNum:
-                        print("["+str(cell.num)+"] ",end='')
-                    else:
-                        print("[#] ",end='')
-                else:
-                    print("[_] ",end='')
-            print('\n')
     def printOut(self):
         print("-----------------------------------")
         for y in range(1,self.size+1):
@@ -82,69 +61,10 @@ class Board:
                     else:
                         print("[_] ",end='')
             print('\n')
-    #return True if new bulb can add here
-    def checkAroundWhite(self,cell):
-        left=self.lookLeft(cell.x, cell.y)
-        right=self.lookRight(cell.x, cell.y)
-        above=self.lookAbove(cell.x, cell.y)
-        below=self.lookBelow(cell.x, cell.y)
-        def checkWhite(val):
-            return True if val.setLight is False else True
-        return checkWhite(left) or checkWhite(right) or checkWhite(above) or checkWhite(below)
-
-    def checkAroundNewBulb(self,cell):
-        left=self.lookLeft(cell.x, cell.y)
-        right=self.lookRight(cell.x, cell.y)
-        above=self.lookAbove(cell.x, cell.y)
-        below=self.lookBelow(cell.x, cell.y)
-        def checkNoneBulb(val,cell):
-            return True if val is None or not val.isBlack or val.checkAddBulb(self) else False
-        return checkNoneBulb(left, cell) and checkNoneBulb(right, cell) and checkNoneBulb(above, cell) and checkNoneBulb(below, cell)
+    
+   
     #return True if there is bulb
-    def checkBulbOnX(self,cell):
-        i=1
-        while(cell.x-i>0):
-            lcell=self.findCell(cell.x-i, cell.y)
-            if lcell is not None:
-                if lcell.isBlack:
-                    break
-                elif lcell.isBulb:
-                    return True
-                    
-            i+=1
-        i=1
-        while(cell.x+i<=self.size):
-            lcell=self.findCell(cell.x+i, cell.y)
-            if lcell is not None:
-                if lcell.isBlack:
-                    break
-                elif lcell.isBulb:
-                    return True
-                    
-            i+=1
-        return False
-    #return True if there is bulb
-    def checkBulbOnY(self,cell):
-        i=1
-        while(cell.y-i>0):
-            lcell=self.findCell(cell.x, cell.y-i)
-            if lcell is not None:
-                if lcell.isBlack:
-                    break
-                elif lcell.isBulb:
-                    return True
-            i+=1
-        #below
-        i=1
-        while(cell.y+i<=self.size):
-            lcell=self.findCell(cell.x, cell.y+i)
-            if lcell is not None:
-                if lcell.isBlack:
-                    break
-                elif lcell.isBulb:
-                    return True
-            i+=1
-        return False
+    
     def setLightNewBulb(self,cell):
         #left
         i=1
@@ -246,45 +166,8 @@ class Black(Cell):
         numOfBulbsAround=sum(map(lambda val: 1 if val is not None and not val.isBlack and val.isBulb else 0 ,[left,right,above,below]))
         return True if (self.isNum and self.num >numOfBulbsAround) else False
 
-    def checkBulbNeed(self,board):
-        if not self.isNum:
-            return []
-        left=board.lookLeft(self.x, self.y)
-        right=board.lookRight(self.x, self.y)
-        above=board.lookAbove(self.x, self.y)
-        below=board.lookBelow(self.x, self.y)
-        whiteCellAround=[]
-        numOfBulbsAround=0
-        for cell in [left,right,above,below]:
-            if cell is not None and not cell.isBlack and cell.isLight is False:
-                whiteCellAround+=[[cell.x,cell.y]]
-            if cell is not None and not cell.isBlack and cell.isBulb:
-                numOfBulbsAround+=1
-        
-        if self.isNum and self.num>numOfBulbsAround:
-            return whiteCellAround
-        else:
-            return []
-    def checkBulbNeed1(self,board):
-        if not self.isNum:
-            return 0
-        left=board.lookLeft(self.x, self.y)
-        right=board.lookRight(self.x, self.y)
-        above=board.lookAbove(self.x, self.y)
-        below=board.lookBelow(self.x, self.y)
-        whiteCellAround=[]
-        numOfBulbsAround=0
-        for cell in [left,right,above,below]:
-            if cell is not None and not cell.isBlack and cell.isLight is False:
-                whiteCellAround+=[[cell.x,cell.y]]
-            if cell is not None and not cell.isBlack and cell.isBulb:
-                numOfBulbsAround+=1
-        if len(whiteCellAround)==0 and self.num-numOfBulbsAround>0:
-            return -1
-        elif self.isNum and self.num>numOfBulbsAround:
-            return 1
-        elif self.isNum and self.num==numOfBulbsAround:
-            return 0
+    
+    
     def checkBulds(self,board):
         if not self.isNum:
             return []
@@ -304,25 +187,7 @@ class Black(Cell):
             return whiteCellAround
         else:
             return []
-    def cellAddBuld(self,board):
-        if not self.isNum:
-            return []
-        left=board.lookLeft(self.x, self.y)
-        right=board.lookRight(self.x, self.y)
-        above=board.lookAbove(self.x, self.y)
-        below=board.lookBelow(self.x, self.y)
-        whiteCellAround=[]
-        numOfBulbsAround=0
-        for cell in [left,right,above,below]:
-            if cell is not None and not cell.isBlack and not cell.isLight:
-                whiteCellAround+=[cell]
-            if cell is not None and not cell.isBlack and cell.isBulb:
-                numOfBulbsAround+=1
-        
-        if self.isNum and self.num>numOfBulbsAround:
-            return whiteCellAround
-        else:
-            return []
+    
         
         
     def numBulbNeed(self,board):
@@ -341,27 +206,7 @@ class Black(Cell):
             return self.num-numOfBulbsAround
         else:
             return -1
-    def enoughWhite(self,board):
-        if not self.isNum:
-            return True
-        left=board.lookLeft(self.x, self.y)
-        right=board.lookRight(self.x, self.y)
-        above=board.lookAbove(self.x, self.y)
-        below=board.lookBelow(self.x, self.y)
-        whiteCellAround=0
-        numOfBulbsAround=0
-        for cell in [left,right,above,below]:
-            if cell is not None and not cell.isBlack and not cell.isLight:
-                whiteCellAround+=1
-            if cell is not None and not cell.isBlack and cell.isBulb:
-                numOfBulbsAround+=1
-        
-        if self.isNum and self.num>numOfBulbsAround:
-            return whiteCellAround>=self.num-numOfBulbsAround
-        elif self.isNum and self.num==numOfBulbsAround:
-            return True
-        else:
-            return False
+    
     
     def dup(self):
         return Black(self.x, self.y, self.num)
@@ -400,29 +245,12 @@ class White(Cell):
         newWhite.isLight=self.isLight
 
         return newWhite
-def AddBulb(x,y,board):
-    print("Thêm đèn ở vị trí ("+str(x)+", "+str(y)+")")
-    board.printOut()
-def Goal():
-    print("Hoàn thành")
-    #a>=b ?
-def CompareHeuristic(a,b):
-    if a[0]>b[0]:
-        return False
-    elif a[0]==b[0]:
-        if a[1]>b[1]:
-            return False
-        else:
-            return True
-    else:
-        return True
+
+
+
 
 def BFS(board):
-    if (board.checkIsGoal()):
-        return board
-    """ 
-        Best-Fist Search
-    """
+    
     ListGeneral=[]
     ListDetail=[]
     ListAddBud=[]
@@ -523,54 +351,15 @@ def BFS(board):
                 return True
             
 
-        
-                
-            
-            
-        
 
-def joinList(list1,list2):
-    a=False
-    for i in list2:
-        for j in list1:
-            if i[0]==j[0] and i[1]==j[1]:
-                a=True
-                break
-        if a:
-            a=False
-            continue
-        else:
-            list1.append(i)
-    return list1
-def removeFromList(list1,list2):
-    a=False
-    temp=[]
-    for i in list1:
-        for j in list2:
-            if i==j:
-                a=True
-                break
-        if a:
-            a=False
-            continue
-        else:
-            temp+=[i]
-    return temp
-
-def stateRecur(board):
-    if board.parent is None:
-        return True
-    if stateRecur(board.parent):
-        AddBulb(board.change[0], board.change[1], board)
-        return True 
 def main():
-    inputBoard=[["w", "w", "0", "w", "w", "w", "w"],
-                ["w", "w", "w", "w", "-1", "w", "w"],
-                ["w", "-1", "w", "w", "w", "w", "2"],
-                ["w", "w", "w", "-1", "w", "w", "w"],
-                ["1", "w", "w", "w", "w", "-1", "w"],
-                ["w", "w", "4", "w", "w", "w", "w"],
-                ["w", "w", "w", "w", "0", "w", "w"]]
+    inputBoard=[["w", "-1", "w", "0", "w", "w", "w"],
+                ["w", "w", "w", "w", "w", "w", "0"],
+                ["w", "w", "w", "w", "w", "w", "w"],
+                ["3", "w", "w", "w", "w", "w", "-1"],
+                ["w", "w", "w", "w", "w", "w", "w"],
+                ["-1", "w", "w", "w", "w", "w", "w"],
+                ["w", "w", "w", "1", "w", "1", "w"]]
     print(len(inputBoard))
     board=Board(len(inputBoard))
     for y in range(0,len(inputBoard)):
@@ -579,17 +368,12 @@ def main():
                 board.addBlackCell(x+1, y+1, int(inputBoard[y][x]))
                 
     board.setRemainWhite()
-    # start = time.time()
+    start = time.time()
     board.printOut()
-    
-
-    # board.createInitState()
-    goal=BFS(board)
-    # end=time.time()
-    # stateRecur(goal)
-    # Goal()
-    # print("Thời gian chạy : ",end-start)
-    # process = psutil.Process(os.getpid())
-    # print("Bộ nhớ đã dùng : ",process.memory_info().rss/ 1024 ** 2,"MB")
+    BFS(board)
+    end=time.time()
+    print("Thời gian chạy : ",end-start)
+    process = psutil.Process(os.getpid())
+    print("Bộ nhớ đã dùng : ",process.memory_info().rss/ 1024 ** 2,"MB")
 
 main()
